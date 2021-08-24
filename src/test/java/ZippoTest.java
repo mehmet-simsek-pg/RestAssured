ea4ac020bdd3dd8b3b161c7f944ae3b400480f06
mehmetsimsek27 <63972963+mehmetsimsek27@users.noreply.github.com>
2021-08-24 21:46:06 +0200

    added test methods

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -1,4 +1,5 @@
 
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
@@ -7,12 +8,78 @@ import static org.hamcrest.Matchers.*;
 public class ZippoTest {
 
     @Test
-    public void test(){
+    public void test() {
         given()
                 .when()
 
                 .then()
 
-                ;
+        ;
+    }
+
+    @Test
+    public void statusCodeTest() {
+        given()
+
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+        ;
+    }
+
+    @Test
+    public void contentTypeTest() {
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body()
+                .contentType(ContentType.JSON)
+        ;
+    }
+
+    @Test
+    public void logTest() {
+        given()
+                .log().all()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body()
+                .contentType(ContentType.JSON)
+
+        ;
+    }
+
+    @Test
+    public void checkStateInResponse() {
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body()
+                .body("country", equalTo("United States"))
+                .statusCode(200)
+        ;
+    }
+
+    @Test
+    public void bodyJsonPathTest() {
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().body()
+                .body("places[0].state", equalTo("California"))
+                .statusCode(200)
+
+        ;
     }
 }
