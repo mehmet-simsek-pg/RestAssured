a1a7a9ebf2981d2d097dda6ac1426d5d0bc4e803
mehmetsimsek27 <63972963+mehmetsimsek27@users.noreply.github.com>
2021-08-24 20:20:56 +0200

    Initial commit

---
+++ src/test/java/ZippoTest.java
@@ -0,0 +1,18 @@
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class ZippoTest {
+
+    @Test
+    public void test(){
+        given()
+                .when()
+
+                .then()
+
+                ;
+    }
+}
