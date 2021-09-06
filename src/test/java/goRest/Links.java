56c0522a18219972df59b2186f947270663a1747
mehmetsimsek27 <63972963+mehmetsimsek27@users.noreply.github.com>
2021-09-06 22:32:17 +0200

    version 1.6

---
+++ src/test/java/goRest/Links.java
@@ -0,0 +1,38 @@
+package goRest;
+
+public class Links {
+    private String previous, current, next;
+
+    public String getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(String previous) {
+        this.previous = previous;
+    }
+
+    public String getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(String current) {
+        this.current = current;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "Links{" +
+                "previous='" + previous + '\'' +
+                ", current='" + current + '\'' +
+                ", next='" + next + '\'' +
+                '}';
+    }
+}
